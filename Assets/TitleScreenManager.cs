d99236a44b2262a7f04e7690c3d70851295ff232
monstebit <78085017+monstebit@users.noreply.github.com>
2024-04-13 18:49:37 +0300

    add start host button

--- Assets/TitleScreenManager.cs
+++ Assets/TitleScreenManager.cs
@@ -1,8 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.UI;
 
-public class TitleScreenManager : MonoBehaviour
+namespace UU
 {
-    
+    public class TitleScreenManager : MonoBehaviour
+    {
+        [SerializeField] private Button _startButton;
+        
+        private void Start()
+        {
+            if (_startButton != null)
+            {
+                _startButton.onClick.AddListener(OnStartButtonHandleClick);
+            }
+        }
+
+        public void StartNetworkAsHost()
+        {
+            NetworkManager.Singleton.StartHost();
+        }
+
+        private void OnStartButtonHandleClick()
+        {
+            StartNetworkAsHost();
+            Debug.Log("HOST HERE");
+        }
+    }
 }
