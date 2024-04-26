d6310f2e6215392af5ee9546cc73f8669892d6c1
monstebit <78085017+monstebit@users.noreply.github.com>
2024-04-26 22:08:03 +0300

    add HandleMovementInput

--- Assets/Scripts/Character/Player/PlayerInputManager.cs
+++ Assets/Scripts/Character/Player/PlayerInputManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,11 +6,13 @@ namespace UU
 {
     public class PlayerInputManager : MonoBehaviour
     {
-        public static PlayerInputManager Instance;
-        
         [SerializeField] private Vector2 _movementInput;
+        [SerializeField] private float _verticalInput;
+        [SerializeField] private float _horizontalInput;
+        [SerializeField] private float _moveAmount;
         
         private PlayerControls _playerControls;
+        public static PlayerInputManager Instance;
 
         private void Awake()
         {
@@ -45,6 +48,11 @@ namespace UU
             _playerControls.Enable();
         }
 
+        private void Update()
+        {
+            HandleMovementInput();
+        }
+
         //  ИЛИ ОТПИСЫВАТЬСЯ ЛУЧШЕ В DISABLE?
         private void OnDestroy()
         {
@@ -61,6 +69,24 @@ namespace UU
             else
             {
                 Instance.enabled = false;
+            }
+        }
+
+        private void HandleMovementInput()
+        {
+            _verticalInput = _movementInput.y;
+            _horizontalInput = _movementInput.x;
+
+            //  ВОВЗРАЩАЕТ АБСОЛЮТНОЕ ЧИСЛО (ЭТО ЗНАЧИТ БЕЗ ЗНАКА МИНУС)
+            _moveAmount = Mathf.Clamp01(Mathf.Abs(_verticalInput) + Mathf.Abs(_horizontalInput));
+
+            if (_moveAmount <= 0.5 && _moveAmount > 0)
+            {
+                _moveAmount = 0.5f;
+            }
+            else if (_moveAmount > 0.5 && _moveAmount <= 1)
+            {
+                _moveAmount = 1;
             }
         }
     }
