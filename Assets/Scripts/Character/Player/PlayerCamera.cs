d71e9fd326767b0ad2c2ba76a740f6cc5901723c
monstebit <78085017+monstebit@users.noreply.github.com>
2024-05-01 23:02:16 +0300

    add player camera

---
+++ Assets/Scripts/Character/Player/PlayerCamera.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+namespace UU
+{
+    public class PlayerCamera : MonoBehaviour
+    {
+        public static PlayerCamera Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
